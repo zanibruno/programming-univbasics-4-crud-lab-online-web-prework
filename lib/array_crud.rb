82f88dfc172f7480ffe53f7ce65aace316423220
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-08-28 12:04:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+ ev_cars = ["model3", "modelS", "modelX", "roadster"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array [1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[4] = "totally"
 end
